22ce8f429cf29aa98438b58906ae48e9074274cd
starlightcoder <tolliam.e@gmail.com>
2026-01-12 17:19:00 +0100

    refactor(alertmanager): remove unused Dockerfile and entrypoint script

--- infra/prometheus/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute environment variables in template
-envsubst < /etc/alertmanager/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
-
-# Start alertmanager with provided arguments
-exec /bin/alertmanager "$@"
